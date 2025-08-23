f6a341d10800f82a2d3c2301308a183c63fc3b38
Mohamed Shendy <mohamedshendy@Shendys-MacBook-Air.local>
2025-08-23 17:10:14 +0300

    chore: bootstrap repo structure

---
+++ iOSTaskYassir/iOSTaskYassirApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOSTaskYassirApp.swift
+//  iOSTaskYassir
+//
+//  Created by Mohamed Shendy on 23/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct iOSTaskYassirApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
